6a556ad318d729b92cceb12d82d3fc35f8036ca6
Rob <tiker-github-239819@funkymonkey.org>
2017-10-20 21:43:02 -0400

    Added support for X52 Ice

--- ankiNodeUtils.js
+++ ankiNodeUtils.js
@@ -23,7 +23,7 @@ noble.on('stateChange', function(state) {
     setTimeout(function() {
        console.log("Stop scanning");
        noble.stopScanning();
-     }, 2000);
+     }, 2000);shoc
   } else {
     console.log("Stop scanning");
     noble.stopScanning();
@@ -85,6 +85,9 @@ noble.on('discover', function(peripheral) {
           break;
         case 16: // Truck - X52
           var carName = "X52"
+          break;
+        case 17: // Truck - X52 Ice
+          var carName = "X52 Ice"
           break;
         default:
           break;
@@ -224,7 +227,8 @@ var connectCar = function(carName) {
      carName == "Ground Shock" ||
      carName == "Nuke" ||
      carName == "Big Bang" ||
-     carName == "Free Wheel") {
+     carName == "Free Wheel" ||
+     carName == "X52 Ice") {
     for(var i=0; i<carList.length;i++) {
       if(carList[i].carName == carName) {
         peripheral = peripheralList[i];
